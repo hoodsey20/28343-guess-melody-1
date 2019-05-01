4feabc32fcfb9deb444147e3da3ea1bfd2122f9b
hoodsey20 <hoodsey20@gmail.com>
2019-05-01 19:31:17 +0700

    добавил экран приветствия

---
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WelcomeScreen = () => {
+  return (
+    <section>
+      <h2>Привет!</h2>
+    </section>
+  );
+};
+
+export default WelcomeScreen;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+import WelcomeScreen from './components/welcome-screen/welcome-screen.jsx';
+
+function init() {
+  ReactDOM.render(
+      <WelcomeScreen />,
+      document.querySelector(`.main`));
+}
+init();
